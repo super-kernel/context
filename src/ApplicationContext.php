290486be3083374f63ef1ad672328204580324ab
wheakerd <wheakerd@gmail.com>
2024-11-27 21:06:15 +0800

    feat(*): add application context about di container

---
+++ src/ApplicationContext.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+namespace SuperKernel\Context;
+
+use Psr\Container\ContainerInterface;
+
+/**
+ * @ApplicationContext
+ * @\ApplicationContext
+ */
+class ApplicationContext
+{
+    static private ContainerInterface $container;
+
+    static public function getContainer(): ContainerInterface
+    {
+        return self::$container;
+    }
+
+    static public function hasContainer(): bool
+    {
+        return isset(self::$container);
+    }
+
+    static public function setContainer(ContainerInterface $container): ContainerInterface
+    {
+        self::$container = $container;
+        return $container;
+    }
+}
