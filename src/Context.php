6f29e147b17c1d4d8323913eebc7be401a557dea
root <wheakerd@gmail.com>
2025-10-11 12:55:12 +0800

    feat: Support thread context.

--- src/Context.php
+++ src/Context.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 namespace SuperKernel\Context;
 
 use Swoole\Coroutine;
+use Swoole\Thread;
+use TypeError;
 
 /**
  * @template TValue
@@ -16,53 +18,74 @@ final class Context
 	private static array $container = [];
 
 	/**
-	 * @param string $id
+	 * @param string $key
 	 * @param TValue $value
 	 *
 	 * @return TValue
 	 */
-	public static function set(string $id, mixed $value): mixed
+	public static function set(string $key, mixed $value): mixed
 	{
-		self::$container[Coroutine::getCid()][$id] = $value;
+		try {
+			self::$container[Thread::getId()][$key] = $value;
+		}
+		catch (TypeError) {
+			self::$container[Coroutine::getCid()][$key] = $value;
+		}
 
 		return $value;
 	}
 
 	/**
-	 * @param string     $id
+	 * @param string $key
 	 * @param TValue $default
 	 *
 	 * @return TValue
 	 */
-	public static function get(string $id, mixed $default = null): mixed
+	public static function get(string $key, mixed $default = null): mixed
 	{
-		return self::$container[Coroutine::getCid()][$id] ?? $default;
+		try {
+			return self::$container[Thread::getId()][$key] ?? $default;
+		}
+		catch (TypeError) {
+			return self::$container[Coroutine::getCid()][$key] ?? $default;
+		}
 	}
 
 	/**
-	 * @param string $id
+	 * @param string $key
 	 *
 	 * @return bool
 	 */
-	public static function has(string $id): bool
+	public static function has(string $key): bool
 	{
-		return array_key_exists($id, self::$container[Coroutine::getCid()] ?? []);
+		try {
+			return array_key_exists($key, self::$container[Thread::getId()] ?? []);
+		}
+		catch (TypeError) {
+		}
+
+		return array_key_exists($key, self::$container[Coroutine::getCid()] ?? []);
 	}
 
 	/**
-	 * @param string|null $id
+	 * @param string|null $key
 	 *
 	 * @return void
 	 */
-	public static function delete(?string $id = null): void
+	public static function delete(?string $key = null): void
 	{
-		$cid = Coroutine::getCid();
+		try {
+			$id = Thread::getId();
+		}
+		catch (TypeError) {
+			$id = Coroutine::getCid();
+		}
 
-		if (null === $id) {
-			unset(self::$container[$cid]);
+		if (null === $key) {
+			unset(self::$container[$id]);
 			return;
 		}
 
-		unset(self::$container[$cid][$id]);
+		unset(self::$container[$id][$key]);
 	}
 }
